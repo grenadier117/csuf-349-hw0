d9a8d55ba5fbbca4b6aa08016ea222eb847789b6
Jake Hamo <jake.hamo@neudesic.com>
2021-09-06 17:31:49 -0700

    updated test suite

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Ottergram header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Ottergram/i);
   expect(linkElement).toBeInTheDocument();
 });
